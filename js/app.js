961668d9010e826aba072424bb90845174ed5828
Gypsyk <gypsyk911@gmail.com>
2016-09-13 19:24:10 +0700

    Commas check

--- js/app.js
+++ js/app.js
@@ -8,18 +8,35 @@ function LC_Controller($scope) {
   
   $scope.checkFood = function () {
 
-  	if($scope.food_list.trim() == '') {
-  		console.log('empty');
+    var food_count = 0;
+    var empty_message = 'Please enter data first';
+    var comma_message = 'Incorrect data';
+
+    $scope.message = null;
+    $scope.chk_class = null;
+
+    //Check if empty
+    if (typeof($scope.food_list) === "undefined" || $scope.food_list.trim() == '') {
+      $scope.food_list = null;
     	$scope.message = empty_message;
+      $scope.chk_class = 'alert alert-danger';
     	return false;
     }
 
+    //Check commas only
+    var commaRe = /^,*$/;
+    var result = commaRe.exec($scope.food_list);
+
+    if(result != null) {
+      $scope.food_list = null;
+      $scope.message = comma_message;
+      $scope.chk_class = 'alert alert-warning';
+      return false; 
+    }
+
     var food_list = $scope.food_list.split(',');
-    var food_count = 0;
-    var empty_message = '<div>Data is empty</div>';
-
-
-
+    console.log('sdas');
+    
     food_list.forEach(function(item, i, arr) {
     	if(item.trim() != '') 
 				food_count++;    		
@@ -28,5 +45,4 @@ function LC_Controller($scope) {
     
   };
 }
-
 })();
